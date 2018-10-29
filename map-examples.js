ffba9a94850d68ba2803b10e163e0f1b9615117d
David <david.hale7@gmail.com>
2018-10-29 15:51:28 -0700

    update map examples

--- map-examples.js
+++ map-examples.js
@@ -6,3 +6,20 @@ var doubledNumbers = [];
 for (var i = 0; i < numbers.length; i++) {
   doubledNumbers.push(numbers[i] * 2);
 }
+
+// map example 1
+
+var doubled = numbers.map(function(number) {
+  return number * 2;
+});
+
+// map example 2 'pluck' example
+
+var cars = [
+  { model: 'Buick', price: 'CHEAP' },
+  { model: 'Camaro', price: 'expensive' }
+];
+
+var prices = cars.map(function(car) {
+  return car.price;
+});
